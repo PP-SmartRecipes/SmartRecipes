9330074396ae135f212691a1cdf3783e34e8ded7
M4T11 <m.galan777@gmail.com>
2020-03-18 13:50:07 +0100

    clickable categories #v1

--- app/src/main/java/com/example/smartrecipes/Category.java
+++ app/src/main/java/com/example/smartrecipes/Category.java
@@ -2,19 +2,35 @@ package com.example.smartrecipes;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class Category extends AppCompatActivity {
+public class Category extends AppCompatActivity implements View.OnClickListener{
+    private CardView breakfastCard, lunchCard, dinnerCard, dessertCard, drinkCard, fastfoodCard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category);
+        //defining Cards
+        breakfastCard = (CardView) findViewById(R.id.breakfast_card);
+        lunchCard = (CardView) findViewById(R.id.lunch_card);
+        dinnerCard = (CardView) findViewById(R.id.dessert_card);
+        drinkCard = (CardView) findViewById(R.id.drink_card);
+        fastfoodCard = (CardView) findViewById(R.id.fastfood_card);
+        //Clicklistener for Cards
+        breakfastCard.setOnClickListener(this);
+        lunchCard.setOnClickListener(this);
+        dinnerCard.setOnClickListener(this);
+        drinkCard.setOnClickListener(this);
+        fastfoodCard.setOnClickListener(this);
+
 
         //Inizialize and assign variable
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
@@ -41,5 +57,14 @@ public class Category extends AppCompatActivity {
                 return false;
             }
         });
+    }
+    //Testowo
+    @Override
+    public void onClick(View v) {
+        Intent i;
+
+        switch (v.getId()) {
+            case R.id.breakfast_card : i = new Intent(this, History.class);startActivity(i); break;
+        }
     }
 }
