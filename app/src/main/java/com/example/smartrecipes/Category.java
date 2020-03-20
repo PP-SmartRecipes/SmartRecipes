c46d716a61bbd5105abf1da1acb6e06e16051d56
M4T11 <m.galan777@gmail.com>
2020-03-20 16:59:11 +0100

    clickable categories #v2

--- app/src/main/java/com/example/smartrecipes/Category.java
+++ app/src/main/java/com/example/smartrecipes/Category.java
@@ -21,13 +21,15 @@ public class Category extends AppCompatActivity implements View.OnClickListener{
         //defining Cards
         breakfastCard = (CardView) findViewById(R.id.breakfast_card);
         lunchCard = (CardView) findViewById(R.id.lunch_card);
-        dinnerCard = (CardView) findViewById(R.id.dessert_card);
+        dinnerCard = (CardView) findViewById(R.id.dinner_card);
+        dessertCard = (CardView) findViewById(R.id.dessert_card);
         drinkCard = (CardView) findViewById(R.id.drink_card);
         fastfoodCard = (CardView) findViewById(R.id.fastfood_card);
         //Clicklistener for Cards
         breakfastCard.setOnClickListener(this);
         lunchCard.setOnClickListener(this);
         dinnerCard.setOnClickListener(this);
+        dessertCard.setOnClickListener(this);
         drinkCard.setOnClickListener(this);
         fastfoodCard.setOnClickListener(this);
 
@@ -58,13 +60,17 @@ public class Category extends AppCompatActivity implements View.OnClickListener{
             }
         });
     }
-    //Testowo
     @Override
     public void onClick(View v) {
         Intent i;
 
         switch (v.getId()) {
-            case R.id.breakfast_card : i = new Intent(this, History.class);startActivity(i); break;
+            case R.id.breakfast_card : i = new Intent(this, recipes_list.class); i.putExtra("categoryString", "breakfast"); startActivity(i); break;
+            case R.id.lunch_card : i = new Intent(this, recipes_list.class); i.putExtra("categoryString", "lunch"); startActivity(i); break;
+            case R.id.dinner_card : i = new Intent(this, recipes_list.class); i.putExtra("categoryString", "dinner"); startActivity(i); break;
+            case R.id.dessert_card : i = new Intent(this, recipes_list.class); i.putExtra("categoryString", "dessert"); startActivity(i); break;
+            case R.id.drink_card : i = new Intent(this, recipes_list.class); i.putExtra("categoryString", "drink"); startActivity(i); break;
+            case R.id.fastfood_card : i = new Intent(this, recipes_list.class); i.putExtra("categoryString", "fastfood"); startActivity(i); break;
         }
     }
 }
